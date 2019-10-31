fc22284f908aad28bb21513e9d3e2f5d737245a8
Karol Dowbecki <karol@dowbecki.com>
2019-10-31 18:00:02 +0000

    Updating JMH and Gradle

---
+++ poligon-jmh/src/jmh/java/poligon/MyBenchmark.java
@@ -0,0 +1,20 @@
+package poligon;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 10, time = 200, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 20, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+public class MyBenchmark {
+
+  @Benchmark
+  public int myScenario() {
+    return 42;
+  }
+
+}
+
