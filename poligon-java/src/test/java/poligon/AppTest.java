31f4b196360e6da0d9e252b4eaa41823637c22f2
Karol Dowbecki <karol@dowbecki.com>
2020-02-25 11:07:49 +0000

    Adding Mockito

--- poligon-java/src/test/java/poligon/AppTest.java
+++ poligon-java/src/test/java/poligon/AppTest.java
@@ -1,16 +1,24 @@
 package poligon;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(MockitoExtension.class)
 class AppTest {
 
-	private App sut = new App();
+	@Mock
+	Object mock;
+
+	App sut = new App();
 
 	@Test
 	void shouldSupportLombok() {
 		assertThat(sut.getVersion()).isNotBlank();
+		assertThat(mock).isNotNull();
 	}
 
 }
