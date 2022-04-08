9ce921f54e612298e4a2a3dce0214abd3841d44b
Karol Dowbecki <karol@dowbecki.com>
2022-04-08 20:09:35 +0100

    Updating Java and Gradle

--- poligon-java/build.gradle.kts
+++ poligon-java/build.gradle.kts
@@ -1,31 +1,37 @@
 plugins {
-    java
-    application
-    id("io.freefair.lombok") version "6.0.0-m2"
+  java
+  application
+  id("io.freefair.lombok") version "6.4.2"
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
-    testImplementation("org.assertj:assertj-core:3.19.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.10.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+  testImplementation("org.assertj:assertj-core:3.22.0")
+  testImplementation("org.mockito:mockito-junit-jupiter:4.4.0")
 }
 
 tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
 }
 
 application {
-    mainClass.set("poligon.App")
+  mainClass.set("poligon.App")
+}
+
+tasks.withType<Jar> {
+  manifest {
+    attributes["Main-Class"] = "poligon.App"
+  }
 }
--- poligon-kotlin/build.gradle.kts
+++ poligon-kotlin/build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.5.20-M1")
-    application
+  id("org.jetbrains.kotlin.jvm").version("1.6.20")
+  application
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 application {
-    mainClass.set("poligon.AppKt")
+  mainClass.set("poligon.AppKt")
 }
